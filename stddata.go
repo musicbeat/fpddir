bfb69ac00098600257cfd438b8f1ad0889561e04
Craig Thomas <craig.thomas@bhnetwork.com>
2014-07-16 21:31:10 -0700

    added a start for documentation of the stddata package. added iso3166.txt source for country codes.

---
+++ stddata.go
@@ -0,0 +1,18 @@
+// Copyright 2014 Musicbeat.com. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package stddata provides a simple mechanism for making
+information about "standard" data sets available
+for lookups and queries. The standards are:
+	Federal Reserve E-Payments Routing Directory
+	ISO 639 Language Codes
+	ISO 4217 Currency Codes
+	ISO 3166-1 Country Codes (Officially Assigned)
+
+Other standards for future work have been identified, including:
+	US Postal Codes and Cities
+	... to be seen
+*/
+package stddata
